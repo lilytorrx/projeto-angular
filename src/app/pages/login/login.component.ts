e7db0e416c3af87b9d805ac26303431264f1bccd
Lídia Torres <repositoriolidia@gmail.com>
2025-09-10 15:59:09 -0300

    feat: Estilização do primeiro input

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
 import { DefaultLoginLayoutComponentComponent } from '../../components/default-login-layout-component/default-login-layout-component.component';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { PrimaryInputComponent } from '../../components/primary-input/primary-input.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    DefaultLoginLayoutComponentComponent
+    DefaultLoginLayoutComponentComponent,
+    ReactiveFormsModule,
+    PrimaryInputComponent
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm!: FormGroup;
+  
+  constructor() {
+    this.loginForm = new FormGroup({
+      email: new FormControl("", [Validators.required, Validators.email]),
+      password: new FormControl("", [Validators.required, Validators.minLength(6)])
+    });
 
-}
+  }
+} 
